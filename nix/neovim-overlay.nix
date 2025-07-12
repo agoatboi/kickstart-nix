314642c95ab1f6d1242891b40382892038f44040
George Katsikas <giorgakis.katsikas@gmail.com>
2025-07-12 16:17:11 +0000

    add hardtime

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -83,6 +83,7 @@ with final.pkgs.lib; let
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
+    hardtime-nvim # disable arrow keys and such
   ];
 
   extraPackages = with pkgs; [
